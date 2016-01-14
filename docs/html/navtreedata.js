67f8e101935de9d90b9c7d585b5fe1f400895276
Sebastian Reiter <s.b.reiter@gmail.com>
2016-01-14 13:38:38 +0100

    updated version to 4.2.7

---
+++ docs/html/navtreedata.js
@@ -0,0 +1,31 @@
+var NAVTREE =
+[
+  [ "ProMesh User Manual", "index.html", [
+    [ "Pages", null, [
+      [ "Home", "index.html", null ],
+      [ "Controls", "pageControls.html", null ],
+      [ "Shortcuts", "pageShortcuts.html", null ],
+      [ "ProMesh License", "pageProMeshLicense.html", null ],
+      [ "External Libraries", "pageExternalLibraries.html", null ],
+      [ "Recent Changes", "pageRecentChanges.html", null ],
+      [ "About", "pageAbout.html", null ],
+      [ "Contact", "pageContact.html", null ]
+    ] ],
+    [ "Script and Tools Reference", null, [
+      [ "Function- and Class-Groups ", "modules.html", "modules" ],
+      [ "Script-Classes", "annotated.html", "annotated" ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"group__promesh__Selection__Vertices.html#gacc1d39414e26927ddeba64b8ce9d69b0",
+"group__ugbase.html#a737feeacb2396d0a7614c07f2303ac00",
+"modules.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ docs/html/search/searchdata.js
@@ -0,0 +1,27 @@
+var indexSectionsWithContent =
+{
+  0: "abcdefghijlmnoprstuv",
+  1: "bcdefgimnrsv",
+  2: "abcdefghijlmprstuv",
+  3: "abcefgimnoprstuv",
+  4: "acehprs"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "functions",
+  3: "groups",
+  4: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Functions",
+  3: "Modules",
+  4: "Pages"
+};
+
